037dbd4b6eb540c267e5161c34ebf1c6e58096a8
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-30 01:26:29 +0200

    Back-end - update GitController update get_pull_requests add repo_url required

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -8,11 +8,19 @@ class GitController extends Controller{
 
     public function get_pull_requests(Request $request){
 
-        $repoUrl = $request->input('repo_url');
+        $repo_url = $request->input('repo_url');
+        if (!$repo_url) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Repository URL is required',
+                "data" => ""
+            ], 400);
+        }
 
         return response()->json([
             'status' => true,
             'message' => 'get pull requests done',
+            "data" => ""
         ], 200);
 
     }
