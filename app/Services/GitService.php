0eda6e46d388467042c4b68bef5ce0894cc45fe6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-30 00:41:47 +0200

    Back-end - update GitService update extract_repo_details add owner,repo

--- app/Services/GitService.php
+++ app/Services/GitService.php
@@ -4,13 +4,17 @@ namespace App\Services;
 
 class GitService{
 
-    public function __construct($repo_url){
+    protected $owner;
+    protected $repo;
 
+    public function __construct($repo_url){
+        $this->extract_repo_details($repo_url);
     }
 
     private function extract_repo_details($repo_url){
         $parts = parse_url($repo_url);
         $path = trim($parts['path'], '/');
+        [$this->owner, $this->repo] = explode('/', $path);
     }
 
 }
