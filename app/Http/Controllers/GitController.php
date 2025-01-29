4ff01ce73e5e3dbc5d832bef665754b1f1a25563
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-30 01:24:41 +0200

    Back-end - update GitController add get_pull_requests

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class GitController extends Controller
-{
-    public function test(){
+class GitController extends Controller{
+
+    public function get_pull_requests(Request $request){
+
+        $repoUrl = $request->input('repo_url');
+
         return response()->json([
             'status' => true,
-            'message' => 'test done',
+            'message' => 'get pull requests done',
         ], 200);
 
     }
