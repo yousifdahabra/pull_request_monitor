4a517f02d0c18e14a183aee95d7f32a631026072
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-31 14:42:03 +0200

    Back-end - update API  add test google

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/get_pull_requests", [GitController::class, "get_pull_requests"]);
 Route::get("/download_files", [GitController::class, "download_files"]);
+Route::get("/test", [GitController::class, "test"]);
