d76aa595f6c4930ab44962f5c5e5734f783ac613
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-02-02 14:00:24 +0200

    Back-end - update API  add sheets google

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\GitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
 
 Route::post("/get_pull_requests", [GitController::class, "get_pull_requests"]);
 Route::get("/download_files", [GitController::class, "download_files"]);
+
+Route::get('/sheets', [GoogleController::class, 'access_google_sheet']);
