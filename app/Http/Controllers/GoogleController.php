6ba6ed6dcec30ac3a6cd1a67ad8960ba125e5a96
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-02-02 14:01:21 +0200

    Back-end - update GoogleController  with access_google_sheet

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Google_Client;
 
-class GoogleController extends Controller
-{
-    //
+class GoogleController extends Controller{
+
+    public function access_google_sheet(){
+
+        $google_client = new Google_Client();
+    }
 }
